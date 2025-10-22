93d013b291c83dbf2e6e27e0a64fd3a6d11c8088
cicamargoba <cicamargoba@unal.edu.co>
2025-10-22 08:11:28 -0500

    Clean up unused wire declarations in femto.v
    Removed unused wire declarations for SPI and UART signals.

--- src/femto.v
+++ src/femto.v
@@ -15,17 +15,7 @@ module tt_um_femto(
   reg resetn;
   /* verilator lint_on SYNCASYNCNET */
   always @(negedge clk) resetn <= rst_n;
-  wire spi_mosi;
-  wire spi_miso;
-  wire spi_cs_n;
-  wire spi_clk;
-  wire spi_mosi_ram;
-  wire spi_miso_ram;
-  wire spi_cs_n_ram;
-  wire spi_clk_ram;
-  wire LEDS;
-  wire RXD;
-  wire TXD;
+
 	
 	femto femto0( 
 		.clk(clk), 
@@ -37,7 +27,7 @@ module tt_um_femto(
 		.spi_clk_ram  (uo_out[4]),		
 		.spi_clk      (uo_out[5]),
 		.LEDS         (uo_out[6]),
-		.TXD          (uo_out[7])
+		.TXD          (uo_out[7]),
 		
 		.spi_miso     (ui_in[0]),
 		.spi_miso_ram (ui_in[1]),
