42bc5484b4013d6c1ccd02992ec0724371222d99
cicamargoba <cicamargoba@unal.edu.co>
2025-10-22 08:46:11 -0500

    Update femto.v

--- src/femto.v
+++ src/femto.v
@@ -1,9 +1,6 @@
 module tt_um_femto(
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
@@ -18,8 +15,8 @@ module tt_um_femto(
 
 	
 	femto femto0( 
-		.clk(clk), 
-		.resetn(resetn), 
+		.clk          (clk), 
+		.resetn       (resetn), 
 		.spi_mosi     (uo_out[0]),
 		.spi_mosi_ram (uo_out[1]),
 		.spi_cs_n     (uo_out[2]),
